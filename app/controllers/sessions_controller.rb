5286cd2f085c397c199dfb232d8b518915a1f353
Dinesh Waitage <Dinesh Waitage>
2016-05-11 02:24:32 +0530

    My first app in rails

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+ def new
+  end
+  def create 
+  	#p @params[:email]
+    user = User.find_by_email(params[:email])
+
+  		raise @user.inspect
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      render :new
+    end
+  end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
+end
